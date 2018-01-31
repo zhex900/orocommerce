27b0b9dde249db54edac3400efeb4c25be90b855
Jake He <jake.he@visicase.com>
2018-01-31 21:08:07 +1100

    Fixed bugs

--- bin/run.sh
+++ bin/run.sh
@@ -83,18 +83,18 @@ if ! grep -q 'aws_key' /var/www/app/config/parameters.yml; then
     aws_secret: '${AWS_SECRET} /var/www/app/config/parameters.yml
 
 fi
-
-php /var/www/app/console oro:platform:update --force
-
-#clear cache.
-info "Rebuild cache"
-rm -rf ${APP_ROOT}/app/cache/*
-php ${APP_ROOT}/app/console cache:clear --env=prod -vvv
-info "Fix ownership for /var/www/ /srv/app-data/"
-chown -R www-data:www-data ${APP_ROOT} /srv/app-data/
-
-#clear entity config
-php ${APP_ROOT}/app/console oro:entity-extend:update-config
+#
+#php /var/www/app/console oro:platform:update --force
+#
+##clear cache.
+#info "Rebuild cache"
+#rm -rf ${APP_ROOT}/app/cache/*
+#php ${APP_ROOT}/app/console cache:clear --env=prod -vvv
+#info "Fix ownership for /var/www/ /srv/app-data/"
+#chown -R www-data:www-data ${APP_ROOT} /srv/app-data/
+#
+##clear entity config
+#php ${APP_ROOT}/app/console oro:entity-extend:update-config
 
 # add redis config to parameters.yml
 # When docker container is restarted, the redis dns config always get deleted.
